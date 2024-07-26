779ecf088448cb0a7a5327f7fe268b4eb2af6302
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-07-26 10:57:35 +0100

    Added comments for geo-location options.

--- Dfe.Data.Common.Infrastructure.CognitiveSearch/SearchByGeoLocation/Options/GeoLocationOptions.cs
+++ Dfe.Data.Common.Infrastructure.CognitiveSearch/SearchByGeoLocation/Options/GeoLocationOptions.cs
@@ -1,37 +1,37 @@
 ﻿namespace Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByGeoLocation.Options;
 
 /// <summary>
-/// 
+///  Configuration options used to define the properties required to make a successful geo-location search request.
 /// </summary>
 public sealed class GeoLocationOptions
 {
     /// <summary>
-    /// 
+    /// The Uri of the azure mapping service used for geo-location searches.
     /// </summary>
     public string? MapsServiceUri { get; set; }
 
     /// <summary>
-    /// 
+    /// The Uri of the azure mapping service endpoint used for geo-location searches.
     /// </summary>
     public string? SearchEndpointUri { get; set; }
 
     /// <summary>
-    /// 
+    /// The subscription key of the azure mapping service used for geo-location searches.
     /// </summary>
     public string? MapsSubscriptionKey { get; set; }
 
     /// <summary>
-    /// 
+    /// The HttpClient (associated with the geo-location search) request  timeout in hours.
     /// </summary>
     public int RequestTimeOutHours { get; set; }
 
     /// <summary>
-    /// 
+    /// The HttpClient (associated with the geo-location search) request  timeout in minutes.
     /// </summary>
     public int RequestTimeOutMinutes { get; set; }
 
-    // <summary>
-    ///
+    /// <summary>
+    /// The HttpClient (associated with the geo-location search) request  timeout in seconds.
     /// </summary>
     public int RequestTimeOutSeconds { get; set; }
 }
